0c7165e1cc2cf52b1ad9b737e5cc3950014b1da6
yokoko <yoko.syun.1408@gmail.com>
2020-09-05 11:37:39 +0900

    first commit

---
+++ PSMNET/train.sh
@@ -0,0 +1,6 @@
+python main.py --maxdisp 192 \
+               --model basic \
+               --datapath /media/yoko/SSD-PGU3/workspace/datasets/KITTI/data_scene_flow/training/ \
+               --epochs 10 \
+               --epochs 10 \
+               --savemodel ./result
